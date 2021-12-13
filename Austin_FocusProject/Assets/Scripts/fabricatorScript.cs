709a4b270070b56922aa7d51175b02ad9310e5ae
AHDev95 <90920393+AHDev95@users.noreply.github.com>
2021-12-13 08:15:33 -0700

    monday

--- Austin_FocusProject/Assets/Scripts/fabricatorScript.cs
+++ Austin_FocusProject/Assets/Scripts/fabricatorScript.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
 
 public class fabricatorScript : MonoBehaviour
 {
-   [SerializeField]
+    [SerializeField]
     int partCount = 0;
+    [SerializeField]
+    private TextMeshProUGUI FabCountText;
+    [SerializeField]
+    private UnityEvent Fabricate;
+
     private void OnTriggerEnter(Collider other) //add to the part count
     {
         if (other.gameObject.CompareTag("Parts"))
         {
             partCount++;
+            FabCountText.text = partCount.ToString();
         }
 
-     
+     if (partCount == 3)
+        {
+            Fabricate.Invoke();
+        }
     }
 
     private void OnTriggerExit(Collider other) //reduce the part count
@@ -21,6 +32,7 @@ public class fabricatorScript : MonoBehaviour
         if (other.gameObject.CompareTag("Parts"))
         {
             partCount--;
+            FabCountText.text = partCount.ToString();
         }
     }
 
