a059be77265796acd7b4d752e34ed703b42f4883
AHDev95 <90920393+AHDev95@users.noreply.github.com>
2021-12-16 09:16:19 -0700

    back on track

--- Austin_FocusProject/Assets/Scripts/flipTheSwitch.cs
+++ Austin_FocusProject/Assets/Scripts/flipTheSwitch.cs
@@ -13,6 +13,7 @@ public class flipTheSwitch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Debug.Log("started");
         hinge = GetComponent<HingeJoint>();
     }
 
